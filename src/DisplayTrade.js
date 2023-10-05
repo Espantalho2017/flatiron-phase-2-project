f9998460b1a0f1f438fd1aec37fdc52fcffec463
Roscoe Moore <rmoore@peersat.com>
2023-10-05 15:35:22 -0400

    Pulled GET Fetch out of DisplayTrade

--- src/DisplayTrade.js
+++ src/DisplayTrade.js
@@ -2,43 +2,41 @@
 import React, { useState, useEffect } from "react";
 
 
-function DisplayTrade() {
+// function DisplayTrade() {
+
+//   const [data, setData] = useState([]);
+//   const [isLoaded, setIsLoaded] = useState(false);
+
+//   console.log(data)
+
+//   useEffect(() => {
+//     fetch("http://localhost:3030/vita49-ecpri-trades")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data)
+//         setData(data);
+//       });
+//   }, []);
+//   // use an empty dependencies array, so we only run the fetch request ONCE
+
+//   // figure out how to sum the grades into a Total Grade
   
-  const [data, setData] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
 
-  console.log(data)
+//   console.log(data)
 
-  useEffect(() => {
-    fetch("http://localhost:3030/vita49-ecpri-trades")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data)
-        setData(data);
-      });
-  }, []);
-  // use an empty dependencies array, so we only run the fetch request ONCE
+//   const grade1Total = 0
+//   const grade2Total = 0
 
-  // figure out how to sum the grades into a Total Grade
-  
-
-  console.log(data)
-
-  const grade1Total = 0
-  const grade2Total = 0
-
+function DisplayTrade({data}) {
+console.log(data)
   return (
     <div className="App">
         
         <h1> Trade Study Matrix Output </h1>
 
       <tbody>
-        
-
        
 {/* I am trying to output the option names and Total Grades within table */}
-
-
 
         {data.map((item, index) => (
             <table>
@@ -51,7 +49,6 @@ function DisplayTrade() {
         </tr>
 
           <tr key={index}>
-            {/* I want this to be a separate row */}
             <td>{""}</td>
             <td>{item.option1}</td>
             <td>{""}</td>
@@ -60,7 +57,6 @@ function DisplayTrade() {
             </ tr>
 
         <tr>
-            {/* I want this to be a separate row */}
             <td>{item.trade1}</td>
             <td>{""}</td>
             <td>{item.option1Trade1Grade}</td>
@@ -70,7 +66,6 @@ function DisplayTrade() {
 
         <tr>
 
-            {/* I want this to be a separate row */}
             <td>{item.trade2}</td>
             <td>{""}</td>
             <td>{item.option1Trade2Grade}</td>
@@ -80,7 +75,6 @@ function DisplayTrade() {
 
             <tr>
 
-            {/* I want this to be a separate row */}
             <td>{item.trade3}</td>
             <td>{""}</td>
             <td>{item.option1Trade3Grade}</td>
@@ -90,7 +84,6 @@ function DisplayTrade() {
 
             <tr>
 
-            {/* I want this to be a separate row */}
             <td>{item.trade4}</td>
             <td>{""}</td>
             <td>{item.option1Trade4Grade}</td>
@@ -99,7 +92,6 @@ function DisplayTrade() {
             </ tr>
 
             <tr>
-            {/* I want this to be a separate row */}
             <td>{item.trade5}</td>
             <td>{""}</td>
             <td>{item.option1Trade5Grade}</td>
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,33 @@
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TradesForm from "./TradesForm"
 import DisplayTrade from "./DisplayTrade"
 
 
 function Home() {
+  
+        const [data, setData] = useState([]);
+        const [isLoaded, setIsLoaded] = useState(false);
+      
+        console.log(data)
+      
+        useEffect(() => {
+          fetch("http://localhost:3030/vita49-ecpri-trades")
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data)
+              setData(data);
+            });
+        }, []);
+        // use an empty dependencies array, so we only run the fetch request ONCE
+          
+        console.log(data)
+
+
+        
     return (
     <div>
-      <DisplayTrade />
+      <DisplayTrade data = {data} />
       <TradesForm />
     </div>
     )
